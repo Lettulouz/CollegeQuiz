3088e3b074e4a017da5e41cc6142b19e3bb8286b
Lettulouz <dominikpiskor1@wp.pl>
2023-03-25 14:25:01 +0100

    Initial commit

---
+++ CollegeQuizWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CollegeQuizWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
