1533f70387d28f017ae74675db6eb4f70c5ef33d
Lettulouz <dominikpiskor1@wp.pl>
2023-03-31 22:15:45 +0200

    Added regexes

---
+++ CollegeQuizWeb/Utils/RegexApp.cs
@@ -0,0 +1,9 @@
+namespace CollegeQuizWeb.Utils;
+
+public static class RegexApp
+{
+    public const string ALL_LETTERS_EXCEPT_NUM_AND_SPEC = @"^[^(\d\[\]!@#$%^&*()_=+{}`~<>/?\\\|;:""]+$";
+    public const string ONLY_SMALL_LETTERS_NUM = @"^[a-z0-9]+$";
+    public const string MIN_ONE_UPPER_LOWER_NUM_SPEC =
+        @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+}
