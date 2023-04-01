ba3899c1af903a2b159091fbafcba83ca7a77ef4
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-04-01 19:10:27 +0200

    minor changes

--- CollegeQuizWeb/Utils/Lang.cs
+++ CollegeQuizWeb/Utils/Lang.cs
@@ -2,26 +2,30 @@ namespace CollegeQuizWeb.Utils;
 
 public static class Lang
 {
-    public const string FirstNameTooShortErrorError = "Podane imię jest za krótkie dla naszej bazy, przepraszamy :(";
-    public const string FirstNameTooLongErrorError = "Podane imię jest za długie dla naszej bazy, przepraszamy :(";
-    public const string FirstNameRegexError = 
+    public const string FIRST_NAME_TOO_SHORT_ERROR = "Podane imię jest za krótkie dla naszej bazy, przepraszamy :(";
+    public const string FIRST_NAME_TOO_LONG_ERROR = "Podane imię jest za długie dla naszej bazy, przepraszamy :(";
+    public const string FIRST_NAME_REGEX_ERROR = 
         "W polu imię znalazły się niedozwolone znaki, usuń je i spróbuj ponownie";
     
-    public const string LastNameTooShortErrorError = "Podane nazwisko jest za krótkie dla naszej bazy, przepraszamy :(";
-    public const string LastNameTooLongErrorError = "Podane nazwisko jest za długie dla naszej bazy, przepraszamy :(";
-    public const string LastNameRegexError = 
+    public const string LAST_NAME_TOO_SHORT_ERROR = "Podane nazwisko jest za krótkie dla naszej bazy, przepraszamy :(";
+    public const string LAST_NAME_TOO_LONG_ERROR = "Podane nazwisko jest za długie dla naszej bazy, przepraszamy :(";
+    public const string LAST_NAME_REGEX_ERROR = 
         "W polu nazwisko znalazły się niedozwolone znaki, usuń je i spróbuj ponownie";
     
-    public const string UsernameTooShortError = "Podany login jest za krótki";
-    public const string UsernameTooLongError = "Podany login jest za długi";
-    public const string UsernameRegexError = "W loginie powinny znaleźć się tylko małe litery oraz cyfry";
+    public const string USERNAME_TOO_SHORT_ERROR = "Podany login jest za krótki";
+    public const string USERNAME_TOO_LONG_ERROR = "Podany login jest za długi";
+    public const string USERNAME_REGEX_ERROR = "W loginie powinny znaleźć się tylko małe litery oraz cyfry";
     
-    public const string EmailNotCorrectError = "Wpisana treść nie jest emailem";
-    public const string EmailTooLongError = "Podana treść nie spełnia założeń emaila";
+    public const string EMAIL_INCORRECT_ERROR = "Wpisana treść nie jest emailem";
+    public const string EMAIL_TOO_LONG_ERROR = "Podana treść nie spełnia założeń emaila";
     
-    public const string PasswordTooShortError = "Podano za krótkie hasło";
-    public const string PasswordTooLongError = "Podano za długie hasło";
-    public const string PasswordRegexError =
+    public const string PASSWORD_TO_SHORT_ERROR = "Podano za krótkie hasło";
+    public const string PASSWORD_REGEX_ERROR =
         "Hasło powinno zawierać co najmniej jedną dużą literę, jedną małą literę, " +
         "jedną cyfrę oraz jeden znak specjalny";
+
+    public const string USERNAME_OR_EMAIL_REQUIRED = "Pole email/nazwa użytkownika jest wymanage";
+    public const string USERNAME_OR_EMAIL_INVALID_CHARS = "Pole email/nazwa użytkownika zawiera niedozwolone znaki";
+    public const string PASSWORD_REQUIRED = "Pole hasła jest wymagane";
+    public const string PASSWORDS_NOT_MATCH = "Podane hasła nie są takie same";
 }
