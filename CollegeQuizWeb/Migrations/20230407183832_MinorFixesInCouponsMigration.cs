d66808c82df3a311d4ca1be800aeddc04e5d50c7
Lettulouz <dominikpiskor1@wp.pl>
2023-04-07 21:56:48 +0200

    Migrations

---
+++ CollegeQuizWeb/Migrations/20230407183832_MinorFixesInCouponsMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeQuizWeb.Migrations
+{
+    public partial class MinorFixesInCouponsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TypeOfSubscription",
+                table: "coupons",
+                newName: "type_of_subscription");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "type_of_subscription",
+                table: "coupons",
+                newName: "TypeOfSubscription");
+        }
+    }
+}
