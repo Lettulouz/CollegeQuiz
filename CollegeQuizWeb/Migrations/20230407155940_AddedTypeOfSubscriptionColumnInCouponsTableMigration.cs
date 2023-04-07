211707d6f13ebd865d62289f817ca7dc9529d220
Lettulouz <dominikpiskor1@wp.pl>
2023-04-07 18:12:44 +0200

    Migrations

---
+++ CollegeQuizWeb/Migrations/20230407155940_AddedTypeOfSubscriptionColumnInCouponsTableMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeQuizWeb.Migrations
+{
+    public partial class AddedTypeOfSubscriptionColumnInCouponsTableMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TypeOfSubscription",
+                table: "coupons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeOfSubscription",
+                table: "coupons");
+        }
+    }
+}
