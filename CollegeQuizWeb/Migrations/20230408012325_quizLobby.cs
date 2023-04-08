8cff4e0d729defefaab8703110dd0ca6b84a02a2
kBaginskik <korniszonba@gmail.com>
2023-04-08 03:30:14 +0200

    quizLobby rejected files

---
+++ CollegeQuizWeb/Migrations/20230408012325_quizLobby.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeQuizWeb.Migrations
+{
+    public partial class quizLobby : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_quiz_lobby_users_user_host_id",
+                table: "quiz_lobby");
+
+            migrationBuilder.DropIndex(
+                name: "IX_quiz_lobby_user_host_id",
+                table: "quiz_lobby");
+
+            migrationBuilder.DropColumn(
+                name: "user_host_id",
+                table: "quiz_lobby");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "user_host_id",
+                table: "quiz_lobby",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_quiz_lobby_user_host_id",
+                table: "quiz_lobby",
+                column: "user_host_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_quiz_lobby_users_user_host_id",
+                table: "quiz_lobby",
+                column: "user_host_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
