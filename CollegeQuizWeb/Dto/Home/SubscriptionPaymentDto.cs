60f95dca9ab3587dd138f547dd10ca4846c69be3
Lettulouz <dominikpiskor1@wp.pl>
2023-04-15 00:49:21 +0200

    Subscription payment with PayU implementation, history included

--- CollegeQuizWeb/Dto/Home/SubscriptionPaymentDto.cs
+++ CollegeQuizWeb/Dto/Home/SubscriptionPaymentDto.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using CollegeQuizWeb.Controllers;
+using CollegeQuizWeb.Utils;
 
 namespace CollegeQuizWeb.Dto.Home;
 
@@ -12,32 +13,38 @@ public class SubscriptionPaymentDto
     public string? Username { get; set; }
     public string? Email { get; set; }
     
-    [Required]
     [DataType(DataType.PhoneNumber)]
-    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
-    public string PhoneNumber { get; set; }
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$", ErrorMessage = Lang.INVALID_PHONE_NUMBER)]
+    public string? PhoneNumber { get; set; }
     
-    [Required]
-    [StringLength(40)]
+    [Required(ErrorMessage = Lang.ADDRESS1_IS_REQUIRED)]
+    [StringLength(40, ErrorMessage = Lang.ADDRESS1_TOO_LONG)]
+    [MinLength(3, ErrorMessage = Lang.ADDRESS1_TOO_SHORT)]
     public string Address1 { get; set; }
     
-    [Required]
     [StringLength(40)]
-    public string Address2 { get; set; }
+    public string? Address2 { get; set; }
 
-    [Required]
-    [StringLength(40)]
+    [Required(ErrorMessage = Lang.STATE_IS_REQUIRED)]
+    [StringLength(40, ErrorMessage = Lang.STATE_TOO_LONG)]
+    [MinLength(3, ErrorMessage = Lang.STATE_TOO_SHORT)]
     public string State { get; set; }
     
-    [Required]
-    [StringLength(40)]
+    [Required(ErrorMessage = Lang.COUNTRY_IS_REQUIRED)]
+    [StringLength(40, ErrorMessage = Lang.COUNTRY_TOO_LONG)]
+    [MinLength(3, ErrorMessage = Lang.COUNTRY_TOO_SHORT)]
     public string Country { get; set; }
+    
+    public int ForWho { get; set; }
+    
+    public bool RememberAddressForLater { get; set; }
+    public int SubscriptionType { get; set; }
 }
 
-public class SubscriptionPaymentDtoPayloader : AbstractControllerPayloader<HomeController>
+public class SubscriptionPaymentDtoPayload : AbstractControllerPayloader<HomeController>
 {
     public SubscriptionPaymentDto Dto { get; set; }
     
-    public SubscriptionPaymentDtoPayloader(HomeController controllerReference)
+    public SubscriptionPaymentDtoPayload(HomeController controllerReference)
         : base(controllerReference) { }
 }
