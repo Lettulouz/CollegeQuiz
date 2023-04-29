b3ceddc14fc25e953a55b935ca2d0850bd82b005
Lettulouz <dominikpiskor1@wp.pl>
2023-04-29 14:22:37 +0200

    Forgotten migrations

---
+++ CollegeQuizWeb/Migrations/20230429112026_AnswersTableUpdate2Migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeQuizWeb.Migrations
+{
+    public partial class AnswersTableUpdate2Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
