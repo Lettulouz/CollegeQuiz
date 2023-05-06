1ac534d4b36a964aed6ef52ba4e13eeba8afa508
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-06 04:57:54 +0200

    minor changes

--- CollegeQuizWeb/webpack.config.js
+++ CollegeQuizWeb/webpack.config.js
@@ -20,9 +20,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
+                use: [ 'babel-loader' ],
             },
             {
                 test: /\.css$/i,
