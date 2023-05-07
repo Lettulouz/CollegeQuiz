91d642ddfea142dfd61ab5a4a26e448573529ff3
Lettulouz <dominikpiskor1@wp.pl>
2023-05-07 23:01:14 +0200

    White fonts

--- CollegeQuizWeb/React/src/quiz-session/QuizQuestionAnswerComponent.jsx
+++ CollegeQuizWeb/React/src/quiz-session/QuizQuestionAnswerComponent.jsx
@@ -51,8 +51,8 @@ const QuizQuestionAnswerComponent = ({ number, isMultiSelect }) => {
                 <button className={`bg-transparent border-0 p-0 m-0 cursor-default ${cssDisabledClickEvent}`}>
                     <img src={ANSWER_SVGS[number]} className="card-img" alt="image_answer_D" />
                     <div className="card-body card-img-overlay d-flex flex-column align-items-center justify-content-center">
-                        <h5 className="card-title text-center">Odpowiedź {ANSWER_LETTERS[number]}</h5>
-                        <p className="card-text text-center">{answers[number]}</p>
+                        <h5 className="card-title text-center text-light">Odpowiedź {ANSWER_LETTERS[number]}</h5>
+                        <p className="card-text text-center text-light">{answers[number]}</p>
                     </div>
                 </button>
             </div>
