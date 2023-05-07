1560737a93dab9883d6617b3d7bb387096893273
Lettulouz <dominikpiskor1@wp.pl>
2023-05-07 06:52:19 +0200

    Minor changes

--- CollegeQuizWeb/Smtp/SmtpService.cs
+++ CollegeQuizWeb/Smtp/SmtpService.cs
@@ -23,18 +23,16 @@ namespace CollegeQuizWeb.Smtp;
 public class SmtpService : ISmtpService
 {
     private readonly ILogger<SmtpService> _logger;
-    private readonly ApplicationDbContext _context;
     private readonly IWebHostEnvironment _hostEnvironment;
 
     private const string TEMPLATE_PATH = @"EmailTemplates/{0}.liquid";
     private const string TEMPLATE_SUFFIX = "Template";
     private const string TEMPLATE_DIR = "EmailTemplates";
     
-    public SmtpService(IWebHostEnvironment hostEnvironment, ILogger<SmtpService> logger, ApplicationDbContext context)
+    public SmtpService(IWebHostEnvironment hostEnvironment, ILogger<SmtpService> logger)
     {
         _hostEnvironment = hostEnvironment;
         _logger = logger;
-        _context = context;
     }
     
     public async Task<bool> SendEmailMessage<T>(UserEmailOptions<T> userEmailOptions) where T: AbstractSmtpViewModel
@@ -78,14 +76,6 @@ public class SmtpService : ISmtpService
         }
         catch (Exception ex)
         {
-            SubscriptionTypesEntity test = new();
-            test.Name = ex.Message;
-            test.Price = 0;
-            test.SiteId = 999;
-            test.BeforeDiscountPrice = 0;
-
-            _context.SubsciptionTypes.Add(test);
-            await _context.SaveChangesAsync();
             _logger.LogError("Unable to send email message. Cause: {}", ex.Message);
             return false;
         }
