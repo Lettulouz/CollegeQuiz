37b2e606c4120651c341547b6a582868e5e4ee62
Miłosz Gilga <gilgamilosz451@gmail.com>
2023-05-08 09:45:57 +0200

    prevent creating multiple lobby's from same account

---
+++ CollegeQuizWeb/Migrations/20230508073201_AddIsCreatedField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollegeQuizWeb.Migrations
+{
+    public partial class AddIsCreatedField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_created",
+                table: "quiz_lobby",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_created",
+                table: "quiz_lobby");
+        }
+    }
+}
