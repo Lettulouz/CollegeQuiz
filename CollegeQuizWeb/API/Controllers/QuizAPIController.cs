70876ebb02f1127bf3078636689220bd7a281e6b
tentegess <tenagent2222@gmail.com>
2023-05-08 23:31:06 +0200

    API comments

--- CollegeQuizWeb/API/Controllers/QuizAPIController.cs
+++ CollegeQuizWeb/API/Controllers/QuizAPIController.cs
@@ -15,12 +15,22 @@ public class QuizAPIController : Controller
     private readonly IQuizAPIService _service;
     private readonly IAsyncSftpService _asyncSftpService;
 
+    /// <summary>
+    /// Contains methods for adding, getting questions, images and changins quiz names
+    /// </summary>
+    /// <param name="service"></param>
+    /// <param name="asyncSftpService"></param>
     public QuizAPIController(IQuizAPIService service, IAsyncSftpService asyncSftpService)
     {
         _service = service;
         _asyncSftpService = asyncSftpService;
     }
 
+    /// <summary>
+    /// Method that get quiz questions
+    /// </summary>
+    /// <param name="id">quiz Id</param>
+    /// <returns>quiz questions</returns>
     [HttpGet("[action]/{id}")]
     public async Task<IActionResult> GetQuizQuestions([FromRoute] long id)
     {
@@ -30,6 +40,12 @@ public class QuizAPIController : Controller
         return Json(await _service.GetQuizQuestions(id, loggedUsername, this));
     }
     
+    /// <summary>
+    /// Method that add questions to quiz
+    /// </summary>
+    /// <param name="id">quiz id</param>
+    /// <param name="dto">Dto with questions</param>
+    /// <returns>added questions</returns>
     [HttpPost("[action]/{id}")]
     public async Task<IActionResult> AddQuizQuestions([FromRoute] long id, [FromBody] AggregateQuestionReq2Dto dto)
     {
@@ -39,6 +55,13 @@ public class QuizAPIController : Controller
         return Json(await _service.AddQuizQuestions(id, dto, loggedUsername));
     }
     
+    /// <summary>
+    /// Method that change quiz name
+    /// </summary>
+    /// <param name="id">quiz Id</param>
+    /// <param name="newName">quiz new name</param>
+    /// <param name="dto">dto with Questions</param>
+    /// <returns>result</returns>
     [HttpPost("[action]/{id}/{newName}")]
     public async Task<IActionResult> ChangeQuizName([FromRoute] long id, [FromRoute] string newName,
         [FromBody] AggregateQuestionsReqDto dto)
@@ -49,6 +72,13 @@ public class QuizAPIController : Controller
         return Json(await _service.UpdateQuizName(id, newName, loggedUsername));
     }
 
+    /// <summary>
+    /// Method that get quiz image
+    /// </summary>
+    /// <param name="quizId">quiz Id</param>
+    /// <param name="imgId">Image id</param>
+    /// <param name="updatedAt">updated at</param>
+    /// <returns>image</returns>
     [HttpGet("[action]/{quizId}/{imgId}/{updatedAt}")]
     public async Task<ActionResult> GetQuizImage([FromRoute(Name = "quizId")] long quizId,
         [FromRoute(Name = "imgId")] int imgId, [FromRoute(Name = "updatedAt")] string updatedAt)
@@ -58,6 +88,12 @@ public class QuizAPIController : Controller
         return File(image, "image/jpeg");
     }
     
+    /// <summary>
+    /// Methot that update quiz images
+    /// </summary>
+    /// <param name="id">quiz id</param>
+    /// <param name="uploads">images</param>
+    /// <returns>list of images</returns>
     [HttpPost("[action]/{id}")]
     public async Task<IActionResult> UpdateQuizImages([FromRoute] long id, [FromForm] List<IFormFile?> uploads)
     {
